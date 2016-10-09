c3f5f8349d108eac871612cd5509b48d5dcbc676
Marta <Marta@HP_Familiar>
2016-10-09 21:22:09 +0200

    ejercicio 1

--- Ejercicio1/src/CalcPrimeNumbersThread.java
+++ Ejercicio1/src/CalcPrimeNumbersThread.java
@@ -1,4 +1,24 @@
+import java.util.Iterator;
+import java.util.List;
 
 public class CalcPrimeNumbersThread extends Thread {
-	// COMPLETAR AQUÍ EL EJERCICIO
+	// COMPLETAR AQUÃ� EL EJERCICIO
+	int num1, num2;
+	public CalcPrimeNumbersThread(int n1, int n2){
+		num1 = n1;
+		num2 = n2;
+	}
+	
+	public void run(){
+		PrimeNumbers pm = new PrimeNumbers();
+		
+		List<Integer> listaPrimos = pm.primesInRange(num1,num2);
+		Iterator<Integer> numsPrimos = listaPrimos.iterator();
+		
+		while (numsPrimos.hasNext()){
+			int numero = numsPrimos.next();
+			System.out.print(numero+", ");
+		}
+		
+	}
 }
--- Ejercicio1/src/PrimeNumbersInRange.java
+++ Ejercicio1/src/PrimeNumbersInRange.java
@@ -1,18 +1,62 @@
-// Queremos calcular los números primos entre 2 números y imprimirlos por pantalla.
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// Queremos calcular los nÃºmeros primos entre 2 nÃºmeros y imprimirlos por pantalla.
 //
 //Requisitos:
 //
-//    lee de la entrada estándar 2 números separados por espacio en la primera linea. (Ej: "2 100")
+//    lee de la entrada estÃ¡ndar 2 nÃºmeros separados por espacio en la primera linea. (Ej: "2 100")
 //    Utiliza un thread que realice el trabajo y se encargue de imprimir por pantalla la lista resultante.
-//    Utiliza el método de extender un thread (extends Thread)
-//    El thread principal debe esperar a la finalización del thread trabajador o "worker".
+//    Utiliza el mÃ©todo de extender un thread (extends Thread)
+//    El thread principal debe esperar a la finalizaciÃ³n del thread trabajador o "worker".
 
 
 public class PrimeNumbersInRange {
+	static String numeros;
+	public PrimeNumbersInRange() throws InterruptedException{
+		//se crea un constructor de objetos hilo
+		Hilo h = new Hilo();
+		h.start();
+		h.join();
+		
+		
+	}
 
-	public static void main(String[] args) {
-		// COMPLETA AQUÍ EL EJERCICIO
-
+	public static void main(String[] args) throws IOException, InterruptedException {
+		// COMPLETA AQUÃ� EL EJERCICIO
+		BufferedReader teclado = new BufferedReader (new InputStreamReader (System.in));
+		
+		System.out.println("Introduce dos números separados por un espacio");
+		numeros = teclado.readLine();
+		
+		//System.out.println(numeros);
+		
+		PrimeNumbersInRange pn = new PrimeNumbersInRange();
+		
+	
+		
+	}
+	
+	private class Hilo extends Thread{
+		
+		public void run(){
+			//splitear
+			int n1,n2;
+			System.out.println(numeros);
+			String[] nums = numeros.split(" ");
+			String num1 = nums[0];
+			n1 = Integer.parseInt(num1);
+			String num2 = nums[1];
+			n2 = Integer.parseInt(num2);
+			
+			
+			CalcPrimeNumbersThread cp = new CalcPrimeNumbersThread(n1,n2);
+			cp.start();
+			
+			
+			
+		}
 	}
 
 }
