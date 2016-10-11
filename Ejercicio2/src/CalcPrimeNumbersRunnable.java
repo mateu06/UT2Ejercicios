bff61126180f34265e1dc93b0b28080a70ca455d
Marta <Marta@HP_Familiar>
2016-10-11 18:33:04 +0200

    ejercicio 2

--- Ejercicio2/src/CalcPrimeNumbersRunnable.java
+++ Ejercicio2/src/CalcPrimeNumbersRunnable.java
@@ -1,15 +1,33 @@
+import java.util.Iterator;
+import java.util.List;
 
 public class CalcPrimeNumbersRunnable implements Runnable {
-	private int from;
-	private int to;
+	private int num1;
+	private int num2;
+	
 
 	public void CalcPrimeNumbersRunnable(int from, int to) {
-		this.from = from;
-		this.to= to;
+		this.num1 = from;
+		this.num2= to;
     }
+	
+	public CalcPrimeNumbersRunnable(int n1, int n2){
+		num1 = n1;
+		num2 = n2;
+	}
+	
 	@Override
 	public void run() {
-		// COMPLETAR AQUÍ EL EJERCICIO
+		// COMPLETAR AQUÃ� EL EJERCICIO
+PrimeNumbers pm = new PrimeNumbers();
+		
+		List<Integer> listaPrimos = pm.primesInRange(num1,num2);
+		Iterator<Integer> numsPrimos = listaPrimos.iterator();
+		
+		while (numsPrimos.hasNext()){
+			int numero = numsPrimos.next();
+			System.out.print(numero+", ");
+		}
 	}
 
 }
--- Ejercicio2/src/PrimeNumbersInRange.java
+++ Ejercicio2/src/PrimeNumbersInRange.java
@@ -1,18 +1,46 @@
-// Queremos calcular los números primos entre 2 números y imprimirlos por pantalla.
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+// Queremos calcular los nÃºmeros primos entre 2 nÃºmeros y imprimirlos por pantalla.
 //
 //Requisitos:
 //
-//    lee de la entrada estándar 2 números separados por espacio en la primera linea. (Ej: "2 100")
+//    lee de la entrada estÃ¡ndar 2 nÃºmeros separados por espacio en la primera linea. (Ej: "2 100")
 //    Utiliza un thread que realice el trabajo y se encargue de imprimir por pantalla la lista resultante.
-//    Utiliza el método de implementar Runnable.
-//    El thread principal debe esperar a la finalización del thread trabajador o "worker".
+//    Utiliza el mÃ©todo de implementar Runnable.
+//    El thread principal debe esperar a la finalizaciÃ³n del thread trabajador o "worker".
 
 
-public class PrimeNumbersInRange {
+public class PrimeNumbersInRange implements Runnable{
+	static String numeros;
 
-	public static void main(String[] args) {
-		// COMPLETA AQUÍ EL EJERCICIO
+	
+	public static void main(String[] args) throws IOException, InterruptedException {
+		// COMPLETA AQUÃ� EL EJERCICIO
+BufferedReader teclado = new BufferedReader (new InputStreamReader (System.in));
+		
+		System.out.println("Introduce dos números separados por un espacio");
+		numeros = teclado.readLine();
+		
+		PrimeNumbersInRange pn = new PrimeNumbersInRange();
+		(new Thread(new PrimeNumbersInRange())).start();
+	}
 
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		int n1,n2;
+		System.out.println(numeros);
+		String[] nums = numeros.split(" ");
+		String num1 = nums[0];
+		n1 = Integer.parseInt(num1);
+		String num2 = nums[1];
+		n2 = Integer.parseInt(num2);
+		
+		(new Thread(new CalcPrimeNumbersRunnable(n1,n2))).start();
+		
 	}
 
 }
